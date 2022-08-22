9ef93ac3ecfaa5766ca3692144ad5bd6643d75ac
JulieChoJulie <h32cho@gmail.com>
2022-08-22 15:35:23 -0700

    routes added

--- front/client.tsx
+++ front/client.tsx
@@ -2,25 +2,12 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import React from 'react';
 import { render } from 'react-dom';
+import App from '@layouts/App';
 import { BrowserRouter } from 'react-router-dom';
-import axios from 'axios';
-import SWRDevtools from '@jjordy/swr-devtools';
 
-import App from './layouts/App';
-
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL =
-  process.env.NODE_ENV === 'production' ? 'https://sleact.nodebird.com' : 'http://localhost:3090';
-console.log('env', process.env.NODE_ENV === 'production');
 render(
   <BrowserRouter>
-    {process.env.NODE_ENV === 'production' ? (
-      <App />
-    ) : (
-      <SWRDevtools>
-        <App />
-      </SWRDevtools>
-    )}
+    <App />
   </BrowserRouter>,
   document.querySelector('#app'),
 );
--- front/layouts/App/index.tsx
+++ front/layouts/App/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import LogIn from '@pages/LogIn';
+import SignUp from '@pages/SignUp';
 
 const App = () => {
-  return <div>Dinner time</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<LogIn />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 };
 
 export default App;
