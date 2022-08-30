0eb0dafff2e14adf2d51749e1ee95085b0f4b545
JulieChoJulie <h32cho@gmail.com>
2022-08-30 11:16:05 -0700

    add chat component

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { VFC } from 'react';
+import { IDM } from '@typings/db';
 import { ChatZone, Section } from './styles';
+import Chat from '@components/Chat';
 
-const ChatList = () => {
+interface Props {
+  chatData?: IDM[];
+}
+
+const ChatList: VFC<Props> = ({ chatData }) => {
   return (
     <ChatZone>
       <Section>section</Section>
+      {chatData?.map((c: IDM) => (
+        <Chat key={c.id} data={c} />
+      ))}
     </ChatZone>
   );
 };
