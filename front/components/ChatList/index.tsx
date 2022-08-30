b3638f9029ef7d65680d9a10156ceebed691f20f
JulieChoJulie <h32cho@gmail.com>
2022-08-30 11:28:34 -0700

    add dayjs and scrollbar

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,19 +1,24 @@
-import React, { VFC } from 'react';
+import React, { useCallback, useRef, VFC } from 'react';
 import { IDM } from '@typings/db';
 import { ChatZone, Section } from './styles';
 import Chat from '@components/Chat';
+import { Scrollbars } from 'react-custom-scrollbars-2';
 
 interface Props {
   chatData?: IDM[];
 }
 
 const ChatList: VFC<Props> = ({ chatData }) => {
+  const scrollbarRef = useRef(null);
+  const onScroll = useCallback(() => {}, []);
   return (
     <ChatZone>
-      <Section>section</Section>
-      {chatData?.map((c: IDM) => (
-        <Chat key={c.id} data={c} />
-      ))}
+      <Scrollbars autoHide ref={scrollbarRef}>
+        <Section>section</Section>
+        {chatData?.map((c: IDM) => (
+          <Chat key={c.id} data={c} />
+        ))}
+      </Scrollbars>
     </ChatZone>
   );
 };
